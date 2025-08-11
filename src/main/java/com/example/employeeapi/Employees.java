d5b13f71bded48f43be82bf811307e03d633070a
Gowshitha Reddy <gowshithareddy@Gowshithas-MacBook-Air.local>
2025-08-11 16:15:08 -0400

    updated post request

--- src/main/java/com/example/employeeapi/Employees.java
+++ src/main/java/com/example/employeeapi/Employees.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class Employees {
     private List<Employee> Employees;
 
+    public Employees() {}
+
+
     public Employees(List<Employee> employees) {
         this.Employees = employees;
     }
